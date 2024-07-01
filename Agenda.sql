9d607b8aa8940cd3cb266c95ea18db73448efcf1
Alan0Y <103816205+Alan0Y@users.noreply.github.com>
2024-06-30 23:32:02 -0600

    Add files via upload

---
+++ Agenda.sql
@@ -0,0 +1,10 @@
+create database Agenda;
+use Agenda;
+create table Datos(
+id int (5) not null, 
+Nombre varchar(10) not null,
+Apellidos varchar(10) not null,
+Direccion varchar(10) not null,
+Telefono varchar(10) not null,
+
+primary key (id));
